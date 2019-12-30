61928cf5bbc30b92bdb6db35ff13a1e82e98cd8c
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-30 15:18:15 -0700

    Finish iteration 1

---
+++ test/activity_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/activity'
+
+class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_attributes
+    activity = Activity.new("Brunch")
+    expected = {}
+
+    assert_equal "Brunch", activity.name
+    assert_equal expected, activity.participants
+  end
+
+  def test_can_add_participant
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal ({"Maria" => 20}), activity.participants
+  end
+
+  def test_can_find_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal 20, activity.total_cost
+
+    activity.add_participant("Luther", 40)
+
+    assert_equal 60, activity.total_cost
+  end
+end
